9f173801d3140feb69c551df2c9ab61b15d336da
Drevanoorschot <drevoorschot@casema.nl>
2017-08-12 22:19:33 +0000

    Add bootstrap theme (w.i.p.) and added debug lines in RandomColor.php

--- RandomColor.php
+++ RandomColor.php
@@ -11,6 +11,8 @@ $blue = rand(0, 1023);
 $sock = socket_create(AF_INET, SOCK_DGRAM, 0);
 $input = $red . ":" . $green . ":" . $blue;
 
+echo $input;
+
 socket_sendto($sock, $input , strlen($input) , 0 , $server , $port);
 
 socket_close();
--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js"></script>
 <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap/theme.min.css">
 <link rel="stylesheet" type="text/css" href="style.css">
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
@@ -15,7 +16,7 @@
         <h1>Lodewijck Led Control Panel</h1>
     </div>
     <div>
-        <div class="col-12 content" id="content">
+        <div class="col-sm-12 content" id="content">
             <form method="post" id="form" name="form">
                 <table>
                     <tr>
