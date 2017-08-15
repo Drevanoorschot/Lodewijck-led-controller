4556f1d59ea8a15e08aa269dd21b58ec645286cd
Drevanoorschot <d.h.m.a.vanoorschot@student.utwente.nl>
2017-08-15 17:28:32 +0200

    Fix mobile scale problem

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 <html>
 <head>
 <title>Lodewijckduino</title>
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js"></script>
 <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
